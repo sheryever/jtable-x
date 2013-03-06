0835b9b04924e5d8c538110fe9a40051758ac686
Abu Ali Muhammad Sharjeel <sheryever@hotmail.com>
2013-03-06 10:20:29 +0300

    jquery.jtable.submittingajaxdata allow user to manipulate data befor submiting form data
    Usage
    $('#tablecontaine).jtable({
    title: '....',
    fields: {
    ......
    },
    submittingAjaxData(data){
    ... manipulate here
    return data;
    }
    });

---
+++ dev/extensions/jquery.jtable.submittingajaxdata.js
@@ -0,0 +1,50 @@
+﻿/************************************************************************
+* FOOTER extension for jTable                                           *
+* Author: Abu Ali Muhammad Sharjeel                                     *
+*************************************************************************/
+(function ($) {
+
+    //Reference to base object members
+    var base = {
+        _submitFormUsingAjax: $.hik.jtable.prototype._submitFormUsingAjax,
+    };
+
+
+    //extension members
+    $.extend(true, $.hik.jtable.prototype, {
+
+
+
+        /* OVERRIDES BASE METHOD */
+        _submitFormUsingAjax: function (url, formData, success, error) {
+            var self = this;
+
+            if (self.option().submittingAjaxData) {
+
+                var data = {
+                    record: self._convertQueryStringToObject(formData)
+                };
+
+                data = self.option().submittingAjaxData(data);
+
+                formData = $.param(data.record);
+            }
+            base._submitFormUsingAjax.apply(this, arguments);
+        },
+        
+        _convertQueryStringToObject: function (queryString) {
+            var jsonObj = {};
+            var e,
+                a = /\+/g,
+                r = /([^&=]+)=?([^&]*)/g,
+                d = function (s) { return decodeURIComponent(s.replace(a, " ")); };
+
+            while (e = r.exec(queryString)) {
+                jsonObj[d(e[1])] = d(e[2]);
+            }
+
+            return jsonObj;
+        }
+    });
+
+})(jQuery);
